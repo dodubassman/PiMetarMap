5ded8cd55899ca939f82ef3471e652594fc3c90a
Tristan Caron <t.caron@alsim.com>
2021-11-23 08:25:41 +0100

    Add settings object

---
+++ pmm/settings.py
@@ -0,0 +1,20 @@
+VMC_LEVEL_COLORS = {
+ 0: '#9941d3',
+ 1: '#ff0000',
+ 2: '#ffc000',
+ 3: '#fefe09',
+ 4: '#0cd124',
+ 5: '#00b0f0',
+}
+
+AIRPORTS = (
+ 'LFRS',
+ 'LFRI',
+ 'LFRZ',
+ 'LFOV',
+ 'LFFI',
+ 'LFRN',
+ 'LFJB',
+ 'LFOU',
+)
+
