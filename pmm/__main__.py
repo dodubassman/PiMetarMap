bd2c39c74429d96af2e2797d3a7994bd512ac451
Tristan Caron <t.caron@alsim.com>
2021-11-23 22:01:20 +0100

    Decouple for DI to come

--- pmm/__main__.py
+++ pmm/__main__.py
@@ -1,20 +1,30 @@
 from pmm import settings
+from pmm.metar.provider import ProviderInterface
 from pmm.metar.provider.inmemoryprovider import InMemoryProvider as Provider
-from pmm.plotter import Plot
+from pmm.plotter import PlotterInterface, Plot
 from pmm.plotter.cliplotter import CliPlotter
 
-provider = Provider()
-plots = []
-for icao in settings.AIRPORTS:
-    metar = provider.fetch_metar_by_icao_code(icao)
-    plots.append(
-        Plot(
-            settings.AIRPORTS.index(icao),
-            settings.VMC_LEVEL_COLORS[metar.vmc_level],
-            icao,
-        )
-    )
 
-plotter = CliPlotter()
-plotter.setup()
-plotter.plot_map(plots)
+def main(provider: ProviderInterface, plotter: PlotterInterface, airports: tuple, vmc_level_colors: dict):
+    plots = []
+    for icao in airports:
+        metar = provider.fetch_metar_by_icao_code(icao)
+        plots.append(
+            Plot(
+                airports.index(icao),
+                vmc_level_colors[metar.vmc_level],
+                icao,
+            )
+        )
+
+    plotter.setup()
+    plotter.plot_map(plots)
+
+
+if __name__ == "__main__":
+    main(
+        provider=Provider(),
+        plotter=CliPlotter(),
+        airports=settings.AIRPORTS,
+        vmc_level_colors=settings.VMC_LEVEL_COLORS
+    ),
